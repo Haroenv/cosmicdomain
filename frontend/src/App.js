066357828a7f702e7d66d4438c64fcc46fdccfae
Elmar Aliyev <elmarnaliyev@gmail.com>
2020-03-31 12:27:10 +0400

    single domain page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {Component, useEffect, useState} from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, NavLink, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Domains from "./pages/Domains";
-
+import axios from "axios";
 import SingleDomain from "./components/shared/SingleDomain";
 import DomainsList from "./components/shared/DomainsList";
+import NotFound from "./pages/NotFound";
 
 function App(props) {
    console.log("route", Route);
@@ -14,8 +15,8 @@ function App(props) {
    return (
       <Router>
          <Switch>
-            <Route exact path='/domainslist' render={rProps => <DomainsList {...rProps} />} />
-            <Route exact path='/:slug' render={rProps => <SingleDomain {...rProps} />} />
+            <Route exact path='/home' render={rProps => <DomainsList {...rProps} />} />
+            <Route exact path='/domains/:slug' render={rProps => <SingleDomain {...rProps} />} />
          </Switch>
          <Route exact path='/' component={Home} />
          <Route exact path='/domains/' component={Domains} />
--- frontend/src/components/shared/SingleDomain.js
+++ frontend/src/components/shared/SingleDomain.js
@@ -9,21 +9,16 @@ const SingleDomain = props => {
    useEffect(() => {
       axios.get(`http://127.0.0.1:8000/api/${props.match.params.slug}`).then(res => {
          setDomain(res.data);
+         console.log("Response Data: ", res.data);
       });
    }, []);
 
+   console.log("Data: ", domain);
+
    return (
-      <div>
-         Domain:
-         {domain.map(dom => {
-            return (
-               <>
-                  <h1>{dom.name}</h1>
-                  <p>eye color: {dom.price}</p>
-               </>
-            );
-         })}
-         <Link to='/home'>back to home</Link>
+      <div className='text-center'>
+         <h1>{domain.name}</h1>
+         <p>{domain.price}</p>
       </div>
    );
 };
